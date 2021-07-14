77be9fc3e53d6529ca23d820fb5a35f5a5528b2d
Alex Fourmy <alexfourmy@gmail.com>
2021-07-14 09:24:52 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,2 @@
-// Common JS Module syntax
-// const person = require('./mymodule1')
 
-// ES2015 Module
-
-// import { person, sayHello } from './mymodule2'
-// import * as mod from './mymodule2'
-
-// console.log(mod.person.name)
-// console.log(mod.sayHello())
-
-import greeting from './mymodule2'
-
-console.log(greeting)
+console.log('hello world')
