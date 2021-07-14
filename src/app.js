65aa863644bb42802095b7a0b31b63a093d9a343
Alex Fourmy <alexfourmy@gmail.com>
2021-07-14 10:29:29 +0200

    Implemented UI post display functionality

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
+import { http } from './http';
+import { ui } from './ui'
 
-console.log('hello world')
+
+// Get posts on DOM load
+document.addEventListener('DOMContentLoaded', getPosts)
+
+function getPosts() {
+  http.get('http://localhost:3000/posts')
+  .then(data => ui.showPosts(data))
+  .catch(err => console.log(err))
+}
+
