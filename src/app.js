7f62413a51ef2ea6c175e6c53393b79ce5b39540
Alex Fourmy <alexfourmy@gmail.com>
2021-07-14 11:55:19 +0200

    completed the create, remove, update and post functionality. Project is complete

--- src/app.js
+++ src/app.js
@@ -11,6 +11,12 @@ document.querySelector('.post-submit').addEventListener('click', submitPost)
 // Listen for delete
 document.querySelector('#posts').addEventListener('click', deletePost)
 
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit)
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit)
+
 // Get posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -23,20 +29,42 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value
   const body = document.querySelector('#body').value
+  const id = document.querySelector('#id').value
 
   const data = {
     title,
     body
   }
 
-  // Create post
-  http.post('http://localhost:3000/posts', data)
-  .then(data => {
-    ui.showAlert('Post added', 'alert alert-success')
-    ui.clearFields()
-    getPosts()
-  })
-  .catch(err => console.log(err))
+  // Validate input
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger')
+  } else {
+    // Check for id
+    if(id === '') {
+      // Create a post
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success')
+        ui.clearFields()
+        getPosts()
+      })
+      .catch(err => console.log(err))
+    } else {
+      // Update post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post updated', 'alert alert-success')
+        ui.changeFormState('add')
+        getPosts()
+      })
+      .catch(err => console.log(err))
+    }
+
+  
+    
+  }
+
 }
 
 // Delete Post
@@ -55,3 +83,30 @@ function deletePost(e) {
   }
 }
 
+// Enable Edit State
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id
+    const body = e.target.parentElement.previousElementSibling.textContent
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent
+
+    const data = {
+      id,
+      title,
+      body
+    }
+    // Fill form with current post
+    ui.fillForm(data)
+  }
+
+  e.preventDefault()
+}
+
+// Cancel edit state
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add')
+  }
+
+  e.preventDefault()
+}
